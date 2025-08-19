7ff270561ebc3243bb147b50d1c8dc554baba2a2
Liu Chia Yi <eason714@gmail.com>
2025-08-19 10:23:40 +0800

    Update app.py
    FastAPI

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 import asyncio
 from utils import test_crawl_and_notify
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"status":"ok",
-            "usage": "GET /test-crawl 啟動 -test模式活動爬蟲推播(支援臺灣三大熱門平台)，環境變數需設 TG_BOT_TOKEN, TG_CHAT_ID"}
+async def index():
+    return {
+        "status": "ok",
+        "usage": "GET /test-crawl 啟動測試模式爬蟲推播"
+    }
 
 @app.get("/test-crawl")
-async def trigger_test_crawler(request: Request):
-    # 正式環境請加密/權限保護
+async def trigger_test_crawler():
     result = await test_crawl_and_notify()
     return result
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=int(8000), reload=True)
